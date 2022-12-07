f0ea011a39009b262e3ac8366b057882dc1cb3f0
Casper Bramming-Hansen <105041881+CBramming@users.noreply.github.com>
2022-12-07 12:34:00 +0100

    Lokale Manager Tests

--- RestOgTestsTests1/Managers/LokaleManagerTests.cs
+++ RestOgTestsTests1/Managers/LokaleManagerTests.cs
@@ -5,40 +5,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RestOgTests.Models;
 
 namespace RestOgTests.Managers.Tests
 {
     [TestClass()]
     public class LokaleManagerTests
     {
+        LokaleManager manager = new LokaleManager();
+
+
         [TestMethod()]
         public void GetAllTest()
         {
-            Assert.Fail();
+            List<Lokale> lokaler = manager.GetAll();
+            Assert.IsNotNull(lokaler);
+            Assert.AreEqual(2, lokaler.Count());
         }
 
         [TestMethod()]
         public void GetByIdTest()
         {
-            Assert.Fail();
+            Lokale lokaler = manager.GetById("D2.08");
+            Assert.AreEqual(true, lokaler.Occupied);
+            Assert.IsNotNull(manager.GetById("D2.08"));
         }
+
+
 
         [TestMethod()]
         public void AddTest()
         {
-            Assert.Fail();
+            Lokale newLokale = new Lokale { LokaleId = "D3.11", Occupied = false };
+            Lokale addedLokale = manager.Add(newLokale);
+            Assert.AreEqual("D3.11", addedLokale.LokaleId);
+            Assert.AreEqual(3, manager.GetAll().Count);
         }
 
         [TestMethod()]
         public void UpdateTest()
         {
-            Assert.Fail();
+            Lokale updates = new Lokale { LokaleId = "D2.09", Occupied = false };
+            Lokale updatedLokale = manager.Update("D2.08", updates);
+            Assert.AreEqual(false, updatedLokale.Occupied);
+            Assert.AreEqual("D2.09", updatedLokale.LokaleId);
         }
 
         [TestMethod()]
         public void DeleteTest()
         {
-            Assert.Fail();
+            Lokale deletedLokale = manager.Delete("D3.11");
+            Assert.AreEqual("D3.11", deletedLokale.LokaleId);
+            Assert.AreEqual(2, manager.GetAll().Count);
         }
+
     }
 }
