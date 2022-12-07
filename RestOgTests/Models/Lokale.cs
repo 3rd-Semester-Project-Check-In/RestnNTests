0be5e5409a89c644233b7cff459abb34abafcd7c
Casper Bramming-Hansen <105041881+CBramming@users.noreply.github.com>
2022-12-07 10:04:00 +0100

    Models Test

---
+++ RestOgTests/Models/Lokale.cs
@@ -0,0 +1,53 @@
+﻿namespace RestOgTests.Models
+{
+    public class Lokale
+    {
+        public string LokaleId { get; set; }
+
+        public bool Occupied { get; set; }
+
+        public Lokale(string lokaleId, bool occupied)
+        {
+            LokaleId = lokaleId;
+            Occupied = occupied;
+        }
+
+        public Lokale()
+        {
+
+        }
+
+        public void ValidateLokaleId()
+        {
+            if(LokaleId == null)
+            {
+                throw new IllegalLokaleIdException("Lokale Id can't be null!");
+            }
+            else if (LokaleId.Length < 2)
+            {
+                throw new IllegalLokaleIdException($"LokaleId's length is required to be higher than 1: '{LokaleId.Length}', '{LokaleId}'");
+            }
+            else if (LokaleId.Length >= 255)
+            {
+                throw new IllegalLokaleIdException($"LokaleId's length is required to be lower than 256: '{LokaleId.Length}', '{LokaleId}'");
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateLokaleId();
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(LokaleId)}={LokaleId}, {nameof(Occupied)}={Occupied.ToString()}}}";
+        }
+    }
+    public class IllegalLokaleIdException : Exception
+    {
+        public IllegalLokaleIdException(string message)
+            : base(message)
+        {
+        }
+    }
+}
---
+++ RestOgTestsTests1/Models/LokaleTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestOgTests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestOgTests.Models.Tests
+{
+    [TestClass()]
+    public class LokaleTests
+    {
+        private Lokale LokaleNoFail = new Lokale("D02.08", false);
+        private Lokale LokaleIdNull = new Lokale(null, false);
+        private Lokale LokaleIdLow = new Lokale("", false);
+        private Lokale LokaleIdHigh = new Lokale("pajfæoksajffæokdsajjdfjljfjfjjidsajidfjpefjoifjsapodifjojoifjwapoeifjwaifjaoifjsadjsapodjsadifjdsaifjsadifdsafjsadifheapoifhseufhepoifwphwaefpohsadpfifilhdsfpiusadhfpiusadhfhdfpihdsafiuasdfiuasdhiusahdfiusahdfiudsahfpiuhdsafiusadhfpiudsahfpudsahfpiusadhfiudsahfiudsahfpiusadhfpiusadhfiaushfpisaudhfpiaudshfpisadhfpuadshfpusahdfpusahdfpudsahfpisudafhpaudsfhpiausdhfpiausdhfåpaudshfpusadhfiasdhfaisudfhpisaudhfapsiudhfpisaudfhpiudsahfpiueahfdsahpiusadhfpiausdhfpidsahfpiusHDFPIUDSA", false);
+
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            // Act
+            string str = LokaleNoFail.ToString();
+
+            // Assert
+            Assert.AreEqual("{LokaleId=D02.08, Occupied=False}", str);
+        }
+
+        [TestMethod]
+        public void StandardPropertyTest()
+        {
+            try
+            {
+                // Act
+                LokaleNoFail.Validate();
+            }
+            catch (Exception ex)
+            {
+                // Assert
+                Assert.Fail("Expected no exception, but got: " + ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void ValidateLokaleIdNullTest()
+        {
+            // Assert
+            Assert.ThrowsException<IllegalLokaleIdException>(() => LokaleIdNull.ValidateLokaleId());
+        }
+
+        [TestMethod]
+        public void ValidateLokaleIdLowTest()
+        {
+            // Assert
+            Assert.ThrowsException<IllegalLokaleIdException>(() => LokaleIdLow.ValidateLokaleId());
+        }
+
+        [TestMethod]
+        public void ValidateLokaleIdHighTest()
+        {
+            // Assert
+            Assert.ThrowsException<IllegalLokaleIdException>(() => LokaleIdHigh.ValidateLokaleId());
+        }
+
+        [TestMethod]
+        public void ValidateTest()
+        {
+            LokaleNoFail.Validate();
+            Assert.ThrowsException<IllegalLokaleIdException>(() => LokaleIdLow.Validate());
+            Assert.ThrowsException<IllegalLokaleIdException>(() => LokaleIdHigh.Validate());
+            Assert.ThrowsException<IllegalLokaleIdException>(() => LokaleIdNull.Validate());
+
+        }
+    }
+}
