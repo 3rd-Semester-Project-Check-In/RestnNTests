b46e7d1a8b9d842e922c1e7501393216b0771fa4
Christoffer Tonnesen <chri23a7@edu.zealand.dk>
2022-12-07 13:25:55 +0100

    Controller Done but done done DOD ay ja

--- RestOgTests/Controllers/LokaleController.cs
+++ RestOgTests/Controllers/LokaleController.cs
@@ -1,12 +1,109 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using RestOgTests.Managers;
+using RestOgTests.Models;
 
 namespace RestOgTests.Controllers
 {
+    [EnableCors("AllowAll")]
+    [Route("api/[controller]")]
+    [ApiController]
     public class LokaleController : Controller
     {
-        public IActionResult Index()
+        private LokaleManager _manager = new LokaleManager();
+
+        [EnableCors("AllowAll")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Lokale>> Get()
         {
-            return View();
+            IEnumerable<Lokale> list = _manager.GetAll();
+            if (list == null || list.Count() == 0)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return Ok(list);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public ActionResult<Lokale> Get(string id)
+        {
+            Lokale getLokale = _manager.GetById(id);
+            if (getLokale == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(getLokale);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<Lokale> Post([FromBody] Lokale newLokale)
+        {
+            try
+            {
+                Lokale createdLokale = _manager.Add(newLokale);
+                return Created("/" + createdLokale.LokaleId, createdLokale);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpPut("{id}")]
+        public ActionResult<Lokale> Put(string id, [FromBody] Lokale updates)
+        {
+            try
+            {
+                Lokale updatedLokale = _manager.Update(id, updates);
+                if (updatedLokale == null)
+                {
+                    return NotFound();
+                }
+                return Ok(updatedLokale);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (ArgumentNullException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpDelete("{id}")]
+        public ActionResult<Lokale> Delete(string id)
+        {
+            Lokale deletedLokale = _manager.Delete(id);
+            if (deletedLokale == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(deletedLokale);
+            }
         }
     }
 }
--- RestOgTests/Program.cs
+++ RestOgTests/Program.cs
@@ -1,14 +1,36 @@
+string allowAllPolicy = "AllowAll";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowAllPolicy,
+                              policy =>
+                              {
+                                  policy.AllowAnyOrigin()
+                                  .AllowAnyMethod()
+                                  .AllowAnyHeader();
+                              });
+});
+
 var app = builder.Build();
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
+
+app.UseCors(allowAllPolicy);
 
 app.MapControllers();
 
